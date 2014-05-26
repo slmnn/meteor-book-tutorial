23814cb88fe0a0213490a834d823ca60b355cfc1
slmnn <arto.salminen@gmail.com>
2014-05-26 11:57:47 +0300

    7-3

--- collections/posts.js
+++ collections/posts.js
@@ -1,2 +1,10 @@
 // No var keyword because we want 'Posts' to be global
 Posts = new Meteor.Collection('posts');
+
+// Allow client side posting
+Posts.allow({
+  insert: function(userId, doc) {
+    // only allow posting if you are logged in
+    return !! userId;
+  }
+});
