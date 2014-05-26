953d89a278f37252a9665da13165dde4cf3b8514
slmnn <arto.salminen@gmail.com>
2014-05-26 14:47:23 +0300

    8_4

--- collections/posts.js
+++ collections/posts.js
@@ -1,6 +1,18 @@
 // No var keyword because we want 'Posts' to be global
 Posts = new Meteor.Collection('posts');
 
+Posts.allow({
+  update: ownsDocument,
+  remove: ownsDocument
+});
+
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    // may only edit the following two fields:
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+});
+
 Meteor.methods({
   post: function(postAttributes) {
     var user = Meteor.user(),
